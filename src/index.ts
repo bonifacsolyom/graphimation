4589bcc04436fb085b6bf14b3e3dd3e43447a1e8
Bobobot <bonifac.solyom@gmail.com>
2021-04-17 17:33:10 +0200

    Changed project structure to use webpack instead of gulp

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import * as THREE from './node_modules/three/src/Three.js';
+import * as THREE from 'three';
 
 const scene: THREE.Scene = new THREE.Scene()
 const camera: THREE.PerspectiveCamera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)

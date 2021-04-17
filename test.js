89956a13128c554c3498f2be889be6a1ea426d1c
Bobobot <bonifac.solyom@gmail.com>
2021-04-17 15:22:16 +0200

    Added rotating cube and lines to scene as a test
    - added threejs library
    - also automatized typescript into javascript conversion
    - there's issues with node module imports - the site works, but the compiler marks the threejs import as an error

---
+++ test.js
@@ -0,0 +1,32 @@
+/* Automatically generated from .typescript, do not directly edit */
+
+import * as THREE from './node_modules/three/src/Three.js';
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+//cube
+const cubeGeometry = new THREE.BoxGeometry();
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+//line
+const points = [];
+points.push(new THREE.Vector3(-10, 0, 0));
+points.push(new THREE.Vector3(0, 10, 0));
+points.push(new THREE.Vector3(10, 0, 0));
+const lineGeometry = new THREE.BufferGeometry().setFromPoints(points);
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
+const line = new THREE.Line(lineGeometry, lineMaterial);
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+window.onload = function () {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    scene.add(cube);
+    scene.add(line);
+    camera.position.z = 5;
+    animate();
+};
